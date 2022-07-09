3e3e5c59cbff12af51ab0f1e26289d88b90186a9
mtvbrianking <mtvbrianking@gmail.com>
2022-07-09 14:08:47 +0300

    Read arbitrary commands

--- src/Commands/Lint.php
+++ src/Commands/Lint.php
@@ -3,11 +3,13 @@
 namespace Bmatovu\LaravelXml\Commands;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
 
 class Lint extends Command
 {
+    const XMLLINT_INDENT = '    ';
+
     /**
      * The name and signature of the console command.
      *
@@ -17,7 +19,7 @@ class Lint extends Command
                                 {input : XML file(s) to parse.}
                                 {--o|output= : Save to a given file.}
                                 {--s|schema= : WXS schema to validate against.}
-                                {--c|command= : Pass raw args to the underlying command}';
+                                {--i|indent= : Indent space. Default: 4 spaces.}';
 
     /**
      * The console command description.
@@ -26,35 +28,99 @@ class Lint extends Command
      */
     protected $description = 'Parsing XML files.';
 
+    public function __construct(?string $name = '')
+    {
+        $this->ignoreValidationErrors();
+
+        parent::__construct($name);
+    }
+
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        // php artisan xml:lint demo.xml --command="--noout"
-        // >>> xmllint demo.xml --format --noout
-        // OK
-
         if (PHP_OS_FAMILY !== 'Linux') {
             $this->error('Only tested on Linux OS');
 
             return;
         }
 
-        $commands = ['xmllint', $this->argument('input'), '--format', $this->option('command')];
+        $commands = $this->buildCommands();
 
-        $this->line('>>> '.implode(' ', $commands));
+        if ($this->option('verbose')) {
+            $this->line('>>> '.implode(' ', $commands));
+        }
 
-        $process = new Process($commands);
+        // dd($commands);
+
+        $process = new Process($commands, null, [
+            'XMLLINT_INDENT' => $this->option('indent') ?? self::XMLLINT_INDENT,
+        ]);
+
         $process->run();
 
         // executes after the command finishes
         if (! $process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+            $this->line($process->getErrorOutput());
+
+            return;
         }
 
-        echo $process->getOutput();
+        $this->line($process->getOutput());
+    }
 
-        $this->line('OK');
+    protected function buildCommands()
+    {
+        $commands = [
+            'xmllint',
+            $this->argument('input'),
+            '--format',
+        ];
+
+        if ($output = $this->option('output')) {
+            $commands[] = '--output';
+            $commands[] = $output;
+        }
+
+        if ($schema = $this->option('schema')) {
+            $commands[] = '--schema';
+            $commands[] = $schema;
+        }
+
+        $commands = array_filter($commands);
+
+        return array_merge($commands, $this->getRawOptions());
+    }
+
+    protected function getRawOptions()
+    {
+        $argv = $_SERVER['argv'];
+
+        if (! \in_array('--', $argv, true)) {
+            return [];
+        }
+
+        $argv = \array_slice($argv, array_search('--', $argv, true) + 1);
+
+        $raw = [];
+
+        foreach ($argv as $arg) {
+            if ('--' === $arg) {
+                $raw[] = $arg;
+
+                continue;
+            }
+
+            if (! Str::startsWith($arg, '-')) {
+                $raw[] = $arg;
+
+                continue;
+            }
+
+            $raw = array_merge($raw, explode('=', $arg));
+        }
+
+        return $raw;
     }
 }
